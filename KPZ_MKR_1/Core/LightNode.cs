d6146a251715f585050dbb1c2acb9b5053ef3809
DanilchukMihaylo <ipz243_dmb@student.ztu.edu.ua>
2026-05-10 23:45:03 +0300

    Added Template Method lifecycle hooks

--- KPZ_MKR_1/Core/LightNode.cs
+++ KPZ_MKR_1/Core/LightNode.cs
@@ -1,5 +1,16 @@
 ﻿abstract class LightNode
 {
+    public LightNode()
+    {
+        OnCreated();
+    }
+
+    protected virtual void OnCreated() { }
+
+    protected virtual void OnRendered() { }
+
+    protected virtual void OnChildAdded(LightNode node) { }
+
     public abstract string OuterHTML();
     public abstract string InnerHTML();
 }
